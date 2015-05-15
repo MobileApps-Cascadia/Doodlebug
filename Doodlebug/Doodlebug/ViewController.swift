16207ff82c2e17617509f383dc052d19b5682c11
Daniel Leclair <danielsleclair@yahoo.com>
2015-05-15 16:10:11 -0700

    Added three main screens

---
+++ Doodlebug/Doodlebug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doodlebug
+//
+//  Created by J. Richard Glass on 5/15/15.
+//  Copyright (c) 2015 KLB Construction. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
